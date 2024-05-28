7f73bd767719bc158c2eed82ea8704a7b96a20cf
Frantisek Prokopec <felixmulti00@gmail.com>
2024-05-28 08:05:06 +0200

    Run on 2 machines using cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("cypress");
+// https://github.com/bahmutov/cypress-split
+const cypressSplit = require('cypress-split')
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -15,16 +17,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://socialboosting.com?convert_optout=1',
     //wpstaging.
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    watchForFileChanges: false,
+    followRedirect: true,
+    pageLoadTimeout: 20000,
+    chromeWebSecurity: false,
+    experimentalMemoryManagement: true,
+    experimentalSingleTabRunMode: true,
     setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      // IMPORTANT: return the config object
+      return config
     },
     specPattern: 'cypress/e2e/*.cy.js'
   },
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  watchForFileChanges: false,
-  followRedirect: true,
-  pageLoadTimeout: 20000,
-  chromeWebSecurity: false,
-  experimentalMemoryManagement: true,
-  experimentalSingleTabRunMode: true,
 });
--- prod.config.js
+++ prod.config.js
@@ -1,16 +1,23 @@
 const {defineConfig } = require('cypress')
+// https://github.com/bahmutov/cypress-split
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
     projectId: "4dcma4",
     
     e2e: {
-        baseUrl:"https://www.socialboosting.com?convert_optout=1"
+        baseUrl:"https://www.socialboosting.com?convert_optout=1",
+        viewportHeight: 1080,
+        viewportWidth: 1920,
+        watchForFileChanges: false,
+        followRedirect: true,
+        pageLoadTimeout: 20000,
+        chromeWebSecurity: false,
+        experimentalMemoryManagement:true,
+    setupNodeEvents(on, config) {
+        cypressSplit(on, config)
+        // IMPORTANT: return the config object
+        return config
+      },
     },
-    viewportHeight: 1080,
-    viewportWidth: 1920,
-    watchForFileChanges: false,
-    followRedirect: true,
-    pageLoadTimeout: 20000,
-    chromeWebSecurity: false,
-    experimentalMemoryManagement:true,
 })
